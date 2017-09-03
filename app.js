4a765bc988a83ef9b04470186ce84bfddbf0da5a
xiecg <xiecg53@163.com>
2017-09-03 15:42:59 +0800

    remove test app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('hello world...');
-}).listen(3000);
-
-console.log('server running on http://121.196.200.74:8081/');
