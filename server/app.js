5404e59b75fc6a499eb6b35bb5beb664724f030d
xiecg <xiecg53@163.com>
2017-09-03 15:35:58 +0800

    update package.json

--- server/app.js
+++ server/app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use(function (req, res, next) {
   if( req.cookies.userId ){
